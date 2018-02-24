4bd7cb59b536c5c745ddae2bedf872f23945c75c
Jay <jvelayud@gmail.com>
2018-02-24 15:46:22 -0500

    Fixing link redirect issue

--- src/components/RouteNavItem.js
+++ src/components/RouteNavItem.js
@@ -2,13 +2,18 @@ import React from "react";
 import { Route } from "react-router-dom";
 import { NavItem } from "react-bootstrap";
 
+function onClick(e, history) {
+  e.preventDefault();
+  history.push(e.currentTarget.getAttribute("href"));
+}
+
 export default props =>
   <Route
     path={props.href}
     exact
     children={({ match, history }) =>
       <NavItem
-        onClick={e => history.push(e.currentTarget.getAttribute("href"))}
+        onClick={e => onClick(e, history)}
         {...props}
         active={match ? true : false}
       >
