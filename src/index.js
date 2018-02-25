d04b696c9d1a02fbce82050cc633f29804a8f51d
Jay <jvelayud@gmail.com>
2018-02-24 20:35:56 -0500

    Switching API, S3, and Cognito to Amplify

--- src/index.js
+++ src/index.js
@@ -1,9 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Amplify from "aws-amplify";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
+//import registerServiceWorker from "./registerServiceWorker";
+import config from "./config";
 import "./index.css";
+
+Amplify.configure({
+  Auth: {
+    mandatorySignIn: true,
+    region: config.cognito.REGION,
+    userPoolId: config.cognito.USER_POOL_ID,
+    identityPoolId: config.cognito.IDENTITY_POOL_ID,
+    userPoolWebClientId: config.cognito.APP_CLIENT_ID,
+  },
+  Storage: {
+    bucket: config.s3.BUCKET,
+    region: config.s3.REGION,
+    identityPoolId: config.cognito.IDENTITY_POOL_ID,
+  },
+  API: {
+    endpoints: [
+      {
+        name: "notes",
+        endpoint: config.apiGateway.URL,
+        region: config.apiGateway.REGION,
+      },
+    ]
+  }
+});
 
 ReactDOM.render(
   <Router>
@@ -11,4 +37,4 @@ ReactDOM.render(
   </Router>,
   document.getElementById("root")
 );
-registerServiceWorker();
+//registerServiceWorker();
